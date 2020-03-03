e8d945daf5b401af4ac1008882ddb170ba84ffcb
itsumura-h <dumblepy@gmail.com>
2020-03-03 14:56:25 +0900

    add

--- src/inflection.nim
+++ src/inflection.nim
@@ -1,76 +1,96 @@
 import re, strutils, strformat
 
 let PLURALS = [
+  # Irregular change
+  (re"(?i)(m)an$", "$1en"), # man, woman
+  (re"(?i)(child)$", "$1ren"), # child
+  (re"(?i)(t)ooth$", "$1eeth"), # tooth
+  (re"(?i)(pe)rson$", "$1ople"), #person
+  (re"(?i)(m)ouse$", "$1ice"), # mouse
+  (re"(?i)(g)oose$", "$1eese"), # goose
+  (re"(?i)(f)oot$", "$1eet"), # foot
+  (re"(?i)(^ox$)$", "$1en"), # ox
+  (re"(?i)(cact)us$", "$1i"), # cactus
+
+  # s, x, ch, sh, o -> *es
+  (re"(?i)(canto|hetero|kimono|photo|piano|pro|portico|quarto|radio|video|zero)$", "$1s"), # exc
+  (re"(?i)sis$", "ses"), # exc
+  (re"(?i)(buffal|potat|tomat)o$", "$1oes"), # exc
+  (re"(?i)(alias|status)$", "$1es"), # exc
+  (re"(?i)(octop|vir)i$", "$1i"), # exc
+  (re"(?i)(octop|vir)us$", "$1i"), # exc
+  (re"(?i)^(ax|test)is$", "$1es"), # exc
+  (re"(?i)(s|x|ch|sh|o)$", "$1es"),
+
+  # consonant + y -> consonant + ies
+  (re"(?i)([^aeiouy]|qu)y$", "$1ies"),
+
+  # end 'f', 'fe' -> replace to v and add 'es'
+  (re"(?i)(belief|roof)", "$1s"), #exc
+  (re"(?i)f$", "$1ves"),
+  (re"(?i)fe$", "$1ves"),
+
   (re"(?i)(quiz)$", "$1zes"),
   (re"(?i)^(oxen)$", "$1"),
   (re"(?i)^(ox)$", "$1en"),
   (re"(?i)(m|l)ice$", "$1ice"),
   (re"(?i)(m|l)ouse$", "$1ice"),
   (re"(?i)(matr|vert|ind)(?:ix|ex)$", "$1ices"),
-  (re"(?i)(x|ch|ss|sh)$", "$1es"),
-  (re"(?i)([^aeiouy]|qu)y$", "$1ies"),
   (re"(?i)(hive)$", "$1s"),
-  (re"(?i)([lr])f$", "$1ves"),
-  (re"(?i)([^f])fe$", "$1ves"),
-  (re"(?i)sis$", "ses"),
   (re"(?i)([ti])a$", "$1a"),
   (re"(?i)([ti])um$", "$1a"),
-  (re"(?i)(buffal|potat|tomat)o$", "$1oes"),
-  (re"(?i)(bu)s$", "$1ses"),
-  (re"(?i)(alias|status)$", "$1es"),
-  (re"(?i)(octop|vir)i$", "$1i"),
-  (re"(?i)(octop|vir)us$", "$1i"),
-  (re"(?i)^(ax|test)is$", "$1es"),
-  # Irregular change
-  (re"(?i)man$", "$1men"),
-  (re"(?i)(pe)rson$", "$1ople"),
+
   # not matchs
   (re"(?i)s$", "s"),
   (re"$", "s"),
 ]
 
-# const SINGULARS = [
-#   (re"(?i)(database)s$", "$1"),
-#   (re"(?i)(quiz)zes$", "$1"),
-#   (re"(?i)(matr)ices$", "$1ix"),
-#   (re"(?i)(vert|ind)ices$", "$1ex"),
-#   (re"(?i)^(ox)en", "$1"),
-#   (re"(?i)(alias|status)(es)?$", "$1"),
-#   (re"(?i)(octop|vir)(us|i)$", "$1us"),
-#   (re"(?i)^(a)x[ie]s$", "$1xis"),
-#   (re"(?i)(cris|test)(is|es)$", "$1is"),
-#   (re"(?i)(shoe)s$", "$1"),
-#   (re"(?i)(o)es$", "$1"),
-#   (re"(?i)(bus)(es)?$", "$1"),
-#   (re"(?i)(m|l)ice$", "$1ouse"),
-#   (re"(?i)(x|ch|ss|sh)es$", "$1"),
-#   (re"(?i)(m)ovies$", "$1ovie"),
-#   (re"(?i)(s)eries$", "$1eries"),
-#   (re"(?i)([^aeiouy]|qu)ies$", "$1y"),
-#   (re"(?i)([lr])ves$", "$1f"),
-#   (re"(?i)(tive)s$", "$1"),
-#   (re"(?i)(hive)s$", "$1"),
-#   (re"(?i)([^f])ves$", "$1fe"),
-#   (re"(?i)(t)he(sis|ses)$", "$1hesis"),
-#   (re"(?i)(s)ynop(sis|ses)$", "$1ynopsis"),
-#   (re"(?i)(p)rogno(sis|ses)$", "$1rognosis"),
-#   (re"(?i)(p)arenthe(sis|ses)$", "$1arenthesis"),
-#   (re"(?i)(d)iagno(sis|ses)$", "$1iagnosis"),
-#   (re"(?i)(b)a(sis|ses)$", "$1asis"),
-#   (re"(?i)(a)naly(sis|ses)$", "$1nalysis"),
-#   (re"(?i)([ti])a$", "$1um"),
-#   (re"(?i)(n)ews$", "$1ews"),
-#   (re"(?i)(ss)$", "$1"),
-#   (re"(?i)s$", ""),
-# ]
+let SINGULARS = [
+  (re"(?i)(database)s$", "$1"),
+  (re"(?i)(quiz)zes$", "$1"),
+  (re"(?i)(matr)ices$", "$1ix"),
+  (re"(?i)(vert|ind)ices$", "$1ex"),
+  (re"(?i)^(ox)en", "$1"),
+  (re"(?i)(alias|status)(es)?$", "$1"),
+  (re"(?i)(octop|vir)(us|i)$", "$1us"),
+  (re"(?i)^(a)x[ie]s$", "$1xis"),
+  (re"(?i)(cris|test)(is|es)$", "$1is"),
+  (re"(?i)(shoe)s$", "$1"),
+  (re"(?i)(o)es$", "$1"),
+  (re"(?i)(bus)(es)?$", "$1"),
+  (re"(?i)(m|l)ice$", "$1ouse"),
+  (re"(?i)(x|ch|ss|sh)es$", "$1"),
+  (re"(?i)(m)ovies$", "$1ovie"),
+  (re"(?i)(s)eries$", "$1eries"),
+  (re"(?i)([^aeiouy]|qu)ies$", "$1y"),
+  (re"(?i)([lr])ves$", "$1f"),
+  (re"(?i)(tive)s$", "$1"),
+  (re"(?i)(hive)s$", "$1"),
+  (re"(?i)([^f])ves$", "$1fe"),
+  (re"(?i)(t)he(sis|ses)$", "$1hesis"),
+  (re"(?i)(s)ynop(sis|ses)$", "$1ynopsis"),
+  (re"(?i)(p)rogno(sis|ses)$", "$1rognosis"),
+  (re"(?i)(p)arenthe(sis|ses)$", "$1arenthesis"),
+  (re"(?i)(d)iagno(sis|ses)$", "$1iagnosis"),
+  (re"(?i)(b)a(sis|ses)$", "$1asis"),
+  (re"(?i)(a)naly(sis|ses)$", "$1nalysis"),
+  (re"(?i)([ti])a$", "$1um"),
+  (re"(?i)(n)ews$", "$1ews"),
+  (re"(?i)(ss)$", "$1"),
+  (re"(?i)s$", ""),
+]
 
 const UNCOUNTABLES = [
+  "aircraft",
+  "deer",
   "equipment",
   "fish",
   "information",
   "jeans",
+  "japanese",
   "money",
   "rice",
+  "salmon",
   "series",
   "sheep",
   "species"
@@ -90,6 +110,16 @@ proc pluralize*(word:string): string =
     echo word
     return word
 
-proc singularize(word:string): string =
-  for inflection in UNCOUNTABLES:
-    
+proc singularize*(word:string): string =
+  if word.len == 0 or UNCOUNTABLES.contains(word.toLowerAscii):
+    return word
+  else:
+    for row in SINGULARS:
+      let rule = row[0]
+      let replacement = row[1]
+      if word.find(rule) >= 0:
+        result = word.replacef(rule, replacement)
+        echo result
+        return result
+    echo word
+    return word
--- tests/test.nim
+++ tests/test.nim
@@ -10,26 +10,155 @@ import unittest
 include inflection
 
 suite "pluralize":
-  test "check regular":
-    check "posts" == pluralize("post")
-    check "octopi" == pluralize("octopus")
-    check "sheep" == pluralize("sheep")
-    check "CamelOctopi" == pluralize("CamelOctopus")
+  suite "check regular":
+    test "s, x, ch, o":
+      check  pluralize("bus") == "buses"
+      check  pluralize("Bus") == "Buses"
+
+      check  pluralize("box") == "boxes"
+      check  pluralize("Box") == "Boxes"
+
+      check  pluralize("church") == "churches"
+      check  pluralize("Church") == "Churches"
+
+      check  pluralize("dish") == "dishes"
+      check  pluralize("Dish") == "Dishes"
+
+      check  pluralize("potato") == "potatoes"
+      check  pluralize("Potato") == "Potatoes"
+
+    test "end 'o' but just add 's'":
+      check  pluralize("canto") == "cantos"
+      check  pluralize("Canto") == "Cantos"
+
+      check  pluralize("hetero") == "heteros"
+      check  pluralize("Hetero") == "Heteros"
+
+      check  pluralize("kimono") == "kimonos"
+      check  pluralize("Kimono") == "Kimonos"
+      
+      check  pluralize("photo") == "photos"
+      check  pluralize("Photo") == "Photos"
+      
+      check  pluralize("piano") == "pianos"
+      check  pluralize("Piano") == "Pianos"
+
+      check  pluralize("pro") == "pros"
+      check  pluralize("Pro") == "Pros"
+
+      check  pluralize("portico") == "porticos"
+      check  pluralize("Portico") == "Porticos"
+
+      check  pluralize("quarto") == "quartos"
+      check  pluralize("Quarto") == "Quartos"
+
+      check  pluralize("radio") == "radios"
+      check  pluralize("Radio") == "Radios"
+
+      check  pluralize("video") == "videos"
+      check  pluralize("Video") == "Videos"
+
+      check  pluralize("zero") == "zeros"
+      check  pluralize("Zero") == "Zeros"
+
+    test "consonant + y -> consonant + ies":
+      check  pluralize("city") == "cities"
+      check  pluralize("City") == "Cities"
+
+      check  pluralize("way") == "ways"
+      check  pluralize("Way") == "Ways"
+
+      check  pluralize("dictionary") == "dictionaries"
+      check  pluralize("Dictionary") == "Dictionaries"
+
+      check  pluralize("story") == "stories"
+      check  pluralize("Story") == "Stories"
+
+      check  pluralize("baby") == "babies"
+      check  pluralize("Baby") == "Babies"
+
+    test "end 'f', 'fe' -> replace to v and add 'es'":
+      check pluralize("half") == "halves"
+      check pluralize("Half") == "Halves"
+
+      check pluralize("knife") == "knives"
+      check pluralize("Knife") == "Knives"
+
+      check pluralize("wife") == "wives"
+      check pluralize("Wife") == "Wives"
+
+      check pluralize("life") == "lives"
+      check pluralize("Life") == "Lives"
+
+      check pluralize("elf") == "elves"
+      check pluralize("Elf") == "Elves"
+
+      check pluralize("leaf") == "leaves"
+      check pluralize("Leaf") == "Leaves"
+
+    test "end 'f', 'fe' -> replace to v and add 'es'  Exception":
+      check pluralize("belief") == "beliefs"
+      check pluralize("Belief") == "Beliefs"
+
+      check pluralize("roof") == "roofs"
+      check pluralize("Roof") == "Roofs"
+      
+      check pluralize("proof") == "proofs"
+      check pluralize("Proof") == "Proofs"
+
+    test "octopi":
+      check pluralize("octopus") == "octopi"
+      check pluralize("Octopus") == "Octopi"
+
+    test "CamelOctopi":
+      check pluralize("CamelOctopus") == "CamelOctopi"
   
-  test "irregular":
-    check "men" == pluralize("man")
-    check "women" == pluralize("woman")
-    check "people" == pluralize("person")
+  test "check irregular":
+    check pluralize("man") == "men"
+    check pluralize("Man") == "Men"
 
-  test "pluralize_plurals":
-    check "plurals" == pluralize("plurals")
-    check "Plurals" == pluralize("Plurals")
+    check pluralize("woman") == "women"
+    check pluralize("Woman") == "Women"
 
-  test "pluralize_empty_string":
-    check "" == pluralize("")
+    check pluralize("child") == "children"
+    check pluralize("Child") == "Children"
+
+    check pluralize("tooth") == "teeth"
+    check pluralize("Tooth") == "Teeth"
+
+    check pluralize("person") == "people"
+    check pluralize("Person") == "People"
+
+    check pluralize("mouse") == "mice"
+    check pluralize("Mouse") == "Mice"
+
+    check pluralize("goose") == "geese"
+    check pluralize("Goose") == "Geese"
+
+    check pluralize("foot") == "feet"
+    check pluralize("Foot") == "Feet"
+
+    check pluralize("ox") == "oxen"
+    check pluralize("Ox") == "Oxen"
+
+    check pluralize("potato") == "potatoes"
+    check pluralize("Potato") == "Potatoes"
+
+    check pluralize("tomato") == "tomatoes"
+    check pluralize("Tomato") == "Tomatoes"
+
+    check pluralize("tomato") == "tomatoes"
+    check pluralize("Tomato") == "Tomatoes"
+
+    check pluralize("cactus") == "cacti"
+    check pluralize("Cactus") == "Cacti"
+
+
+  test "empty_string":
+    check pluralize("") == ""
 
   test "uncountability":
     for word in UNCOUNTABLES:
-      assert word == pluralize(word)
+      assert pluralize(word) == word
       # assert word == inflection.singularize(word)
       # assert pluralize(word) == singularize(word)
